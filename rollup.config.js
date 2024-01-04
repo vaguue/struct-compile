492546ed120eb2c5acf0c06a892a5743f207ac51
Seva D <sabrinarempel7@gmail.com>
2024-01-04 21:12:53 +0300

    stay only with Node for some time

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 export default {
   input: 'src/index.js',
@@ -9,12 +7,6 @@ export default {
       file: 'build/index.js',
       format: 'cjs'
     },
-    {
-      file: 'build/index.min.js',
-      format: 'iife',
-      name: 'version',
-      plugins: [nodePolyfills(), terser()]
-    }
   ],
   plugins: [nodeResolve()]
 };
