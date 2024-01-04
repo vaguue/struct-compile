d22f3ad922f433c0e190b8559a0971c1cf2b2da8
Seva D <sabrinarempel7@gmail.com>
2024-01-05 02:27:34 +0300

    currentArch fallback

--- src/currentArch.js
+++ src/currentArch.js
@@ -1,6 +1,19 @@
 import os from 'os';
 
-const is64 = os.machine().includes('64');
+let is64;
+
+if (os.machine) {
+  is64 = os.machine().includes('64');
+}
+else {
+  const arch = os.arch();
+  if (arch.includes('64') || arch == 'mipsel') {
+    is64 = true;
+  }
+  else {
+    is64 = false;
+  }
+}
 
 export const currentArch = {
   endianness: os.endianness(),
